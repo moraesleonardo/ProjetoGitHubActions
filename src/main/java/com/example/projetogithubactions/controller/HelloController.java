2cb5e3f76500e137ede546a956981fdfc114e7e1
Leonardo Moraes <leonardo.moraes@al.infnet.edu.br>
2024-09-21 13:20:13 -0300

    Primeiro commit do projeto do github actions

---
+++ src/main/java/com/example/projetogithubactions/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.projetogithubactions.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Olá Mundo";
+    }
+
+}
---
+++ src/test/java/com/example/projetogithubactions/ProjetoGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
